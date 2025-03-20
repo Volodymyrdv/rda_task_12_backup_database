b18010b029ee13e12edb1148470cc47222bb4b5c
Volodymyrdv <vovakryt123453@gmail.com>
2025-03-20 23:49:43 +0200

    A new solution

--- task.sh
+++ task.sh
@@ -2,5 +2,5 @@
 mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --result-file=backup1.sql
 mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < backup1.sql
 
-mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --no-create-db --result-file=backup2.sql
+mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --no-create-info --result-file=backup2.sql
 mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment < backup2.sql
