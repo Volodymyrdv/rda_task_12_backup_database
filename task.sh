b6d0320e54239e9deb35c1f028bff9fcb61aece8
Volodymyrdv <vovakryt123453@gmail.com>
2025-03-20 23:57:52 +0200

    Create tables

--- task.sh
+++ task.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+mysql -u $DB_USER -p$DB_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ShopDBReserve;"
+mysql -u $DB_USER -p$DB_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ShopDBDevelopment;"
+
 mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --result-file=backup1.sql
 mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < backup1.sql
 
